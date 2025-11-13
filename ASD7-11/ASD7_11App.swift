552b9381d437f2264dab7417a57b7ef19a119752
thehaipe <thehaipe2007@gmail.com>
2025-11-13 06:07:37 +0200

    Initial Commit

---
+++ ASD7-11/ASD7_11App.swift
@@ -0,0 +1,17 @@
+//
+//  ASD7_11App.swift
+//  ASD7-11
+//
+//  Created by Valentyn on 13.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ASD7_11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
